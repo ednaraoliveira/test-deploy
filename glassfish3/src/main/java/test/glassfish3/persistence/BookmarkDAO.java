38ea7e0f1266e31ce0d2172db4b72522faf44198
Ednara Oliveira <ednaraoliveira@gmail.com>
2012-09-26 14:41:40 -0300

    Bookmark para Glassfish3

---
+++ glassfish3/src/main/java/test/glassfish3/persistence/BookmarkDAO.java
@@ -0,0 +1,17 @@
+package test.glassfish3.persistence;
+
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+
+import br.gov.frameworkdemoiselle.stereotype.PersistenceController;
+import br.gov.frameworkdemoiselle.template.JPACrud;
+
+import test.glassfish3.domain.Bookmark;
+
+@PersistenceController
+public class BookmarkDAO extends JPACrud<Bookmark, Long> {
+	
+	private static final long serialVersionUID = 1L;
+	
+}
