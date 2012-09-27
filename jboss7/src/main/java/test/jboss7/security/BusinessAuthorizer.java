780148a1a9e8fa1ed695bd7dd36350ee9720dabb
Ednara Oliveira <ednaraoliveira@gmail.com>
2012-09-27 08:18:01 -0300

    Deploy bookmark para Jboss7

---
+++ jboss7/src/main/java/test/jboss7/security/BusinessAuthorizer.java
@@ -0,0 +1,31 @@
+package test.jboss7.security;
+
+import javax.enterprise.inject.Alternative;
+import javax.inject.Inject;
+
+import br.gov.frameworkdemoiselle.security.Authorizer;
+
+public class BusinessAuthorizer implements Authorizer{
+
+	@Inject
+	private Login login;
+	
+	@Override
+	public boolean hasRole(String role) {
+		if(role.equals(login.getRole())){
+			return true;
+		}else{
+			return false;
+		}		
+	}
+
+	@Override
+	public boolean hasPermission(String resource, String operation) {
+		if (resource.equals("negocio") && operation.equals("metodo2")){
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+}
